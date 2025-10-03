6c6e0e68e3a14733fa0f89a36c1719fa68fbe01a
Wagner Xu <wagnerhsu@hotmail.com>
2025-10-03 16:22:34 +0800

    feat: add files

---
+++ MyCode/02-document/silicon/j-record-manager.py
@@ -0,0 +1,76 @@
+from langchain.indexes import SQLRecordManager, index
+from langchain_postgres.vectorstores import PGVector
+from langchain_openai import OpenAIEmbeddings
+from langchain.docstore.document import Document
+
+connection = "postgresql+psycopg://langchain:langchain@localhost:6024/langchain"
+collection_name = "my_docs"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+base_url = os.environ.get("BASE_URL")
+api_key = os.environ.get("API_KEY")
+model_name = os.environ.get("MODEL")
+
+embeddings_model = OpenAIEmbeddings(model=model_name, base_url=base_url, api_key=api_key)
+namespace = "my_docs_namespace"
+
+vectorstore = PGVector(
+    embeddings=embeddings_model,
+    collection_name=collection_name,
+    connection=connection,
+    use_jsonb=True,
+)
+
+record_manager = SQLRecordManager(
+    namespace,
+    db_url="postgresql+psycopg://langchain:langchain@localhost:6024/langchain",
+)
+
+# Create the schema if it doesn't exist
+record_manager.create_schema()
+
+# Create documents
+docs = [
+    Document(page_content='there are cats in the pond', metadata={
+             "id": 1, "source": "cats.txt"}),
+    Document(page_content='ducks are also found in the pond', metadata={
+             "id": 2, "source": "ducks.txt"}),
+]
+
+# Index the documents
+index_1 = index(
+    docs,
+    record_manager,
+    vectorstore,
+    cleanup="incremental",  # prevent duplicate documents
+    source_id_key="source",  # use the source field as the source_id
+)
+
+print("Index attempt 1:", index_1)
+
+# second time you attempt to index, it will not add the documents again
+index_2 = index(
+    docs,
+    record_manager,
+    vectorstore,
+    cleanup="incremental",
+    source_id_key="source",
+)
+
+print("Index attempt 2:", index_2)
+
+# If we mutate a document, the new version will be written and all old versions sharing the same source will be deleted.
+
+docs[0].page_content = "I just modified this document!"
+
+index_3 = index(
+    docs,
+    record_manager,
+    vectorstore,
+    cleanup="incremental",
+    source_id_key="source",
+)
+
+print("Index attempt 3:", index_3)
